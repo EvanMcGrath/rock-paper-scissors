1915083a2800cc06384db30e5407d9701b58b620
Evan <evan.grath@gmail.com>
2022-06-06 20:18:44 -0400

    Compete gamRound() if else chain

--- myscripts.js
+++ myscripts.js
@@ -5,11 +5,11 @@ function computerPlay() {
     computerPick = Math.floor(Math.random()*3)
     
     if ( computerPick === 0 ) {
-        return "Rock"
+        return "rock"
     } else if ( computerPick === 1 ) {
-        return "Scissors"
+        return "scissors"
     } else {
-        return "Paper"
+        return "paper"
     }
 
 }
@@ -20,10 +20,39 @@ function gameRound (playerSelection, computerSelection) {
     //     return `${playerSelection} vs ${computerSelection}, tie.`
     // }
 
-    let a = playerSelection === "rock" || computerSelection === "rock"
-    let b = playerSelection === "paper" || computerSelection === "paper" 
-    let c = playerSelection === "scissors" || computerPlay === "scissors" 
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose, paper covers rock."
+        } else if (computerSelection === "scissors") {
+            return "You win! Rock smashes scissors!"
+        } else if (computerSelection === "rock") {
+            return "Two rocks make a tie."
+        } else {
+            return "You can't bloody do that mate!"
+        }
+    }
 
-    console.log(a,b,c)
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper covers rock."
+        } else if (computerSelection === "scissors") {
+            return "You lose, scissors cut paper."
+        } else if (computerSelection === "paper") {
+            return "Two papers makes a tie."
+        } else {
+            return "You can't bloody do that mate!"
+        }
+    }
 
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors cut paper."
+        } else if (computerSelection === "rock") {
+            return "You lose, rock smashes scissors!"
+        } else if (computerSelection === "scissors") {
+            return "Two scissors makes a tie."
+        } else {
+            return "You can't bloody do that mate!"
+        }
+    }
 }
