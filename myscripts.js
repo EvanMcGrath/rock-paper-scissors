457e587088a8c82a902ebb793e2cc459b85defe9
Evan <evan.grath@gmail.com>
2022-06-07 18:07:38 -0400

    Change function 'return' to 'result =' variable

--- myscripts.js
+++ myscripts.js
@@ -20,43 +20,48 @@ function gameRound (playerSelection, computerSelection) {
     //     return `${playerSelection} vs ${computerSelection}, tie.`
     // }
 
+
+    // What I think I'll need to use to get the user input from the UI  
+    //let playerSelection = document.getElementById("playerPick").textContent
+
     let caseSelection = playerSelection.toLowerCase() 
+    let result 
 
     if (caseSelection === "rock") {
         if (computerSelection === "paper") {
-            return "You lose, paper covers rock."
+            result = "You lose, paper covers rock."
         } else if (computerSelection === "scissors") {
-            return "You win! Rock smashes scissors!"
+            result = "You win! Rock smashes scissors!"
         } else if (computerSelection === "rock") {
-            return "Two rocks make a tie."
+            result = "Two rocks make a tie."
         } else {
-            return "You can't bloody do that mate!"
+            result = "You can't bloody do that mate!"
         }
     }
 
     if (caseSelection === "paper") {
         if (computerSelection === "rock") {
-            return "You win! Paper covers rock."
+            result = "You win! Paper covers rock."
         } else if (computerSelection === "scissors") {
-            return "You lose, scissors cut paper."
+            result = "You lose, scissors cut paper."
         } else if (computerSelection === "paper") {
-            return "Two papers makes a tie."
+            result = "Two papers makes a tie."
         } else {
-            return "You can't bloody do that mate!"
+            result = "You can't bloody do that mate!"
         }
     }
 
     if (caseSelection === "scissors") {
         if (computerSelection === "paper") {
-            return "You win! Scissors cut paper."
+            result = "You win! Scissors cut paper."
         } else if (computerSelection === "rock") {
-            return "You lose, rock smashes scissors!"
+            result = "You lose, rock smashes scissors!"
         } else if (computerSelection === "scissors") {
-            return "Two scissors makes a tie."
+            result = "Two scissors makes a tie."
         } else {
-            return "You can't bloody do that mate!"
+            result = "You can't bloody do that mate!"
         }
-    }
-}
-// What I think I'll need to use to get the user input from the UI  
-//let playerSelection = document.getElementById("playerPick").textContent
+    } else {
+        result = "You can't bloody do that mate!"
+    }  
+}   
